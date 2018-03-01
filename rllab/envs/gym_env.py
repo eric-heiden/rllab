f3ce523ae0aec602215043dfef64bfc09bc2d2be
Eric Heiden <eric@eric-heiden.de>
2018-03-01 23:16:53 +0000

    OpenAI gym conversion given a gym instance

--- rllab/envs/gym_env.py
+++ rllab/envs/gym_env.py
@@ -56,7 +56,7 @@ class NoVideoSchedule(object):
 
 
 class GymEnv(Env, Serializable):
-    def __init__(self, env_name, record_video=True, video_schedule=None, log_dir=None, record_log=True,
+    def __init__(self, env, record_video=True, video_schedule=None, log_dir=None, record_log=True,
                  force_reset=False):
         if log_dir is None:
             if logger.get_snapshot_dir() is None:
@@ -65,7 +65,11 @@ class GymEnv(Env, Serializable):
                 log_dir = os.path.join(logger.get_snapshot_dir(), "gym_log")
         Serializable.quick_init(self, locals())
 
-        env = gym.envs.make(env_name)
+        if isinstance(env, str):
+            env = gym.envs.make(env)
+
+        assert isinstance(env, gym.Env)
+
         self.env = env
         self.env_id = env.spec.id
 
@@ -86,7 +90,10 @@ class GymEnv(Env, Serializable):
         logger.log("observation space: {}".format(self._observation_space))
         self._action_space = convert_gym_space(env.action_space)
         logger.log("action space: {}".format(self._action_space))
-        self._horizon = env.spec.tags['wrapper_config.TimeLimit.max_episode_steps']
+        if hasattr(env.spec, 'tags') and 'wrapper_config.TimeLimit.max_episode_steps' in env.spec.tags:
+            self._horizon = env.spec.tags['wrapper_config.TimeLimit.max_episode_steps']
+        else:
+            self._horizon = 1000
         self._log_dir = log_dir
         self._force_reset = force_reset
 
